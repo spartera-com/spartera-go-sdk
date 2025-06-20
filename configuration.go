2e1c5560500ca5d59cf6c8c3fe6f7aea7a395e9f
tony-spartera <tony@spartera.com>
2025-06-19 20:46:55 -0400

    Release v1.0.62
    - Auto-generated SDK from Spartera API specification
    - Updated to version 1.0.62
    - Generated on Thu Jun 19 20:46:55 EDT 2025
    
    This SDK is automatically generated from our private monorepo.
    Report issues at: https://github.com/spartera-com/spartera-go-sdk/issues

--- configuration.go
+++ configuration.go
@@ -93,7 +93,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "OpenAPI-Generator/1.0.50/go",
+		UserAgent:        "OpenAPI-Generator/1.0.62/go",
 		Debug:            false,
 		Servers:          ServerConfigurations{
 			{
